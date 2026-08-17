8d110a1807b494096e65192bc629458ac02629c6
thepeakkz <314389124+thepeakkz@users.noreply.github.com>
2026-08-17 15:27:33 +0500

    Переместить лого clot-60 на первое место в слайдерах

--- src/components/ClientLogosBlock.tsx
+++ src/components/ClientLogosBlock.tsx
@@ -27,7 +27,7 @@ function ClientLogo({ ext, id }: { ext: string; id: number }) {
 
 export default function ClientLogosBlock() {
   const baseRow1 = [
-    12, 2, 11, 3, 1, 10, 5, 18, 13, 8, 6, 4, 7, 17, 19, 15, 16, 14, 9,
+    60, 12, 2, 11, 3, 1, 10, 5, 18, 13, 8, 6, 4, 7, 17, 19, 15, 16, 14, 9,
   ];
   const baseRow2 = [
     20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
