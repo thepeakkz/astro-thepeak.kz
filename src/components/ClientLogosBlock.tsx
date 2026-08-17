70606daf4f98dddf84aa8cfd9c994777a57b7894
thepeakkz <314389124+thepeakkz@users.noreply.github.com>
2026-08-17 15:21:19 +0500

    Добавить лого clot-60 в слайдер партнеров и блок клиентов

--- src/components/ClientLogosBlock.tsx
+++ src/components/ClientLogosBlock.tsx
@@ -33,7 +33,7 @@ export default function ClientLogosBlock() {
     20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
   ];
   const baseRow3 = [38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52];
-  const baseRow4 = [53, 54, 55, 56, 57, 58, 59];
+  const baseRow4 = [53, 54, 55, 56, 57, 58, 59, 60];
 
   function createInfiniteItems(baseArray: number[]) {
     const mapped = baseArray.map((id) => ({
